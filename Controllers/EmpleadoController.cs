d32b85a0e0367e5f0ea1de83221c086c1e9f989e
adderlyfer <fernando_segura1@usmp.pe>
2018-11-03 13:26:40 -0500

    cambiosempleado

---
+++ Controllers/EmpleadoController.cs
@@ -0,0 +1,35 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SRVFlorerias.Models;
+
+namespace SRVFlorerias.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+
+         public IActionResult Index()
+        {
+            return View();
+        }
+
+
+   public IActionResult GestionEmpleado()
+        {
+            
+            Empleado obj = new Empleado();
+
+            ViewData["Message"] = "Pagina de Gestión de Empleados.";
+
+            return View();
+        }
+        
+
+    }
+    
+}
