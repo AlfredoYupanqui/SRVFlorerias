b6c6041ea740d389132d63d94a34d931bb83a330
cabsjm1 <jhonatan_sicha@usmp.pe>
2018-11-02 23:20:38 -0500

    Update Producto.cs

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -4,11 +4,16 @@ using System;
 namespace SRVFlorerias.Models
 {
     public class Producto
-    {
+    {   
+        //Id
         public int Id { get; set; }
+        //Tipo
         public string Tipo { get; set; }
+        //Nombre
         public string Nombre { get; set; }
+        //Categoría
         public string Categoria { get; set; }
+        //Precio
         public double Precio { get; set; }
         public double Stock { get; set; }
         public double Dscto { get; set; }
