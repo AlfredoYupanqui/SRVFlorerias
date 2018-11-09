59abbd1006ec9a0375e981e353ac5dd7ebd7aedd
adderlyfer <43500980+adderlyfer@users.noreply.github.com>
2018-11-08 21:40:45 -0600

    Update Empleado.cs

--- Models/Empleado.cs
+++ Models/Empleado.cs
@@ -6,6 +6,9 @@ namespace SRVFlorerias.Models
     public class Empleado
     {
         public int Id { get; set; }
+        public int Nombre { get; set; }
+        public int Apellido { get; set; }
+        public int  Fecha { get; set; }
     }
 }
 
