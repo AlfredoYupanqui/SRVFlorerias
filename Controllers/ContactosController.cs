a567b0ee0999be3ff4027604455257a2ddf0292c
adderlyfer <43500980+adderlyfer@users.noreply.github.com>
2018-11-12 21:54:27 -0500

    cambiocontrollercontactos

--- Controllers/ContactosController.cs
+++ Controllers/ContactosController.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -48,7 +47,7 @@ namespace SRVFlorerias.Models
             _context.Contactos.Remove(contactos);
             _context.SaveChanges();
 
-            
+            return RedirectToAction("Contactos");
         }
 
     }
